56808819d529c7906095bd68fea83e4582a1d6a2
Victoria <reyedvictoria1803@gmail.com>
2024-06-06 12:12:28 -0600

    Initial Commit

---
+++ PracticaStoryBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticaStoryBoard
+//
+//  Created by imac on 10/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
